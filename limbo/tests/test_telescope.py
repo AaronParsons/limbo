91debe4650c1746ae59f744c01ca90e9f25534f6
Radio Lab <aparsons@berkeley.edu>
2023-02-06 17:08:23 -0800

    fixed variable calling error

--- limbo/tests/test_telescope.py
+++ limbo/tests/test_telescope.py
@@ -10,7 +10,7 @@ JD = 2458500.3
 telescope = Telescope()
 
 class TestTelescope(unittest.TestCase):
-    
+
     def test_check_pointing(self):
         with pytest.raises(AssertionError):
             telescope._check_pointing(10, 50)
@@ -20,56 +20,56 @@ class TestTelescope(unittest.TestCase):
             telescope._check_pointing(50, 0)
         with pytest.raises(AssertionError):
             telescope._check_pointing(50, 355)
-        
+
     def test_get_pointing(self):
         alt, az = 60, 110
-        assert(telescope.get_pointing(alt, az) == (59.70, 109.87)) 
-    
+        assert(telescope.get_pointing(alt, az) == (59.70, 109.87))
+
     # pointing tests:
     def test_alt20(self):
         ALT = 20.
         AZ = telescope.AZ_STOW
-        telescope.point(alt, az, wait=True)
+        telescope.point(ALT, AZ, wait=True)
         alt, az = telescope.get_pointing()
         assert alt == pytest.approx(ALT, 1)
-        
+
     def test_alt50(self):
         ALT = 50.
         AZ = telescope.AZ_STOW
-        telescope.point(alt, az, wait=True)
+        telescope.point(ALT, AZ, wait=True)
         alt, az = telescope.get_pointing()
         assert alt == pytest.approx(ALT, 1)
-    
+
     def test_alt75(self):
         ALT = 75.
         AZ = telescope.AZ_STOW
-        telescope.point(alt, az, wait=True)
+        telescope.point(ALT, AZ, wait=True)
         alt, az = telescope.get_pointing()
         assert alt == pytest.approx(ALT, 1)
-        
+
     def test_az300(self):
         ALT = telescope.ALT_STOW
         AZ = 300.
-        telescope.point(alt, az, wait=True)
+        telescope.point(ALT, AZ, wait=True)
         alt, az = telescope.get_pointing()
         assert az == pytest.approx(AZ, 1)
-    
+
     def test_alt225(self):
         ALT = telescope.ALT_STOW
         AZ = 225.
-        telescope.point(alt, az, wait=True)
+        telescope.point(ALT, AZ, wait=True)
         alt, az = telescope.get_pointing()
         assert az == pytest.approx(AZ, 1)
-        
+
     def test_alt35(self):
         ALT = telescope.ALT_STOW
         AZ = 35.
-        telescope.point(alt, az, wait=True)
+        telescope.point(ALT, AZ, wait=True)
         alt, az = telescope.get_pointing()
         assert az == pytest.approx(AZ, 1)
-        
-    def test_sunpos(self): 
+
+    def test_sunpos(self):
         ra, dec = telescope.sunpos(jd=JD)
         assert ra == pytest.approx(298.375, 0)
         assert dec == pytest.approx(-20.8789, 0)
-        
+
