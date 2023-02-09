bacaef7378d3470b1d6d0e455b554c65e2638106
Radio Lab <aparsons@berkeley.edu>
2023-02-08 17:40:31 -0800

    add sun/pointing test script

---
+++ scripts/sun_test.py
@@ -0,0 +1,33 @@
+# This script will determine the position of the Sun
+# and point the telescope to be just ahead of the Sun
+# (approx 5 mins ahead). The Sun will then travel through
+# the telescope's beam.
+# This is a basic pointing test.
+
+import limbo.telescope
+import astropy.units as u
+import subprocess
+import time
+
+t = limbo.telescope.Telescope()
+
+# Add 5 mins in RA
+5ra = 1.25*u.deg # [deg]
+
+# Current Sun RA and DEC
+sunra, sundec = t.sunpos()*u.deg # [deg]
+
+# Compute alt, az that we will point to
+alt, az = t.calc_altaz(sunra+5ra, sundec) # [deg]
+
+# Point telescope
+t.point(alt, az)
+
+# Collect data
+subprocess.run(['/usr/local/bin/enable_record.sh'], shell=True) # enable data recording
+time.sleep(20*60) # collect data for 20 mins
+subprocess.run(['/usr/local/bin/disable_record.sh'], shell=True) # disable data recording
+subprocess.run(['/usr/local/bin/stop_recorder.sh'], shell=True) # stop the data recorder
+
+# Stow when finished
+t.stow()
