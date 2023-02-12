a418933a042ae950ae2d5ca135786e30a70ae190
darbymccauley <darbymccauley@berkeley.edu>
2023-02-12 13:38:51 -0800

    Agilent freq synth control code

---
+++ limbo/agilent.py
@@ -0,0 +1,112 @@
+import socket
+import time
+import thread
+
+DEVICE = '/dev/usbtmc0' # default mounting point
+HOST, PORT = '10.32.92.95', 1341
+WAIT = 0.3 # s
+
+FREQ_UNIT = ['GHz','MHz','kHz']
+AMP_UNIT = ['dBm','mV','uV']
+
+class Synth:
+    """
+    Instantiate connection to the Agilent frequency synthesizer.
+    """
+    def __init__(self, host=HOST, port=PORT, device=DEVICE):
+        self.hostport = (host, port)
+        self.device = device
+
+    def _write(self, cmd):
+        '''Low-level writing interface to device.  Not intended direct use.'''
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.settimeout(10) # seconds
+        self.sock.connect(self.hostport)
+        self.sock.sendall(bytes(cmd, encoding='utf-8'))
+        if not cmd.endswith('?'): self.sock.close()
+
+    def _read(self):
+        '''Low-level reading interface to device.  Not intended direct use.'''
+        resp = self.sock.recv(1024)
+        self.sock.close()
+        return resp
+
+    def get_frequency(self):
+        """
+        Get the current frequency setting for the CW (continuous wave)
+        output mode of the synthesizer.
+        
+        Inputs: None
+        Returns:
+            - freq (float): Numerical frequency setting
+            - unit (str): Units of freq (GHz, MHz, or kHz)
+        """
+        self._write(':FREQuency:CW?')
+        resp = self._read()
+        freq, unit = resp.split()
+        return float(freq), unit
+
+    def set_frequency(self, freq, unit):
+        """ Set the frequency of the CW (continuous wave) output
+        mode of the synthesizer.
+
+        Inputs:
+            - freq (float): Numerical frequency setting
+            - unit (str): Units of freq ('GHz', 'MHz', 'kHz')
+        Returns: None
+        """
+        assert(unit in FREQ_UNIT)
+        cmd = ':FREQuency:CW %f %s' % (val, unit)
+        sef._write(cmd)
+
+    def get_amplitude(self)
+        """
+        Get the current amplitude setting for the CW (continuous wave) 
+        output mode of the synthesizer.
+
+        Inputs: None
+        Returns:
+            - amp (float): Numerical amplitide setting
+            - unit (str): Units of amp ('dBm', 'mV', 'uV')
+        """
+        self._write(':AMPLitude:CW?')
+        resp = self._read()
+        amp, unit = resp.split()
+        return float(amp), unit
+
+   def set_amplitude(self, amp, unit):
+        """
+        Set the amplitude setting for the CW (continuous wave) output 
+        mode of the synthesizer.
+
+        Inputs: 
+            - amp (float): Numerical amplitide setting
+            -unit (str): Units of amp ('dBm', 'mV', 'uV')
+        Returns: None
+        """
+        assert (unit in AMP_UNIT)
+        cmd = ':AMPLitude:CW %f %s' % (amp, unit)
+        self._write(cmd)
+
+    def get_RFout_status(self):
+        """
+        Get the RFout status of the synthesizer.
+        If RFout is on, a '1' is returned. If the RFout is off,
+        a '0' is returned.
+        """
+        self._write(':RFOutput:STATe?')
+        status = self._read()[0] # read first bit
+        if status == '1': return 1
+        elif status == '0': return 0
+
+    def RFout_on(self):
+        """
+        Turn RFout on.
+        """
+        self._write(':RFOutput:STATe ON')
+
+    def RFout_off(self):
+        """
+        Turn RFout off.
+        """
+        self._write(':RFOutput:STATe OFF')
---
+++ limbo/tests/test_agilent.py
@@ -0,0 +1,19 @@
+import pytest
+from limbo.agilent import Synth 
+
+lo = Synth()
+
+def TestAgilent:
+    def test_RFout_on(self):
+        lo.RFout_on()
+        assert(lo._read()[0] == '1')
+    
+    def test_RFout_off(self):
+        lo.RFout_off()
+        assert(lo._read()[0] == '0')
+        
+    def test_get_RFout_status(self):
+        lo.RFout_on()
+        assert(lo.get_RFout_status() == '1')
+        lo.RFout_off()
+        assert(lo.get_RFout_status() == '0')
