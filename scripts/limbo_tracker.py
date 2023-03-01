a24930deb1034e21036e43b0f4b59e9874fcafdb
Darby <darbymccauley@berkeley.edu>
2023-03-01 01:00:02 +0000

    update tracker

--- scripts/limbo_tracker.py
+++ scripts/limbo_tracker.py
@@ -1,12 +1,18 @@
 #! /usr/bin/env python
 
+""" Observing script for limbo. """
+
 import limbo.telescope as telescope
 import subprocess
 import argparse
+import redis
+
+REDISHOST = 'localhost'
+r = redis.Redis(REDISHOST, decode_responses=True)
 
 parser = argparse.ArgumentParser(prog='LIMBO_tracker', description = 'Track an object using the LIMBO/Leuschner telescope system')
 
-parser.add_argument('--object', dest='object', type=str,  help='observing object', choices=['sun', 'sgr1935', 'crab'], default=None, required=False)
+parser.add_argument('--object', dest='object', type=str,  help='observing object', choices=['sun', 'sgr1935', 'crab', 'CygA'], default=None, required=False)
 parser.add_argument('--ra', dest='ra', type=str, help='Right ascension [hms]', default=None, required=False)
 parser.add_argument('--dec', dest='dec', type=str, help='Declination [dms]', default=None, required=False)
 parser.add_argument('--verbose', dest='verbose', type=bool, help='Be verbose', default=False, required=False)
@@ -17,35 +23,42 @@ INPUT_RA = args.ra
 INPUT_DEC = args.dec
 VERBOSE = args.verbose
 
+t = telescope.Telescope()
+
+sources = {
+        'sun':{'RA':t.sunpos()[0], 'DEC':t.sunpos()[1]},
+        'crab':{'RA':telescope.CRAB_RA, 'DEC':telescope.CRAB_DEC},
+        'sgr1935':{'RA':telescope.SGR_RA, 'DEC':telescope.SGR_DEC},
+        'CygA':{'RA':telescope.CYGA_RA, 'DEC':telescope.CYGA_DEC}
+            }
+
 if INPUT_RA is None and INPUT_DEC is None:
-    assert OBJECT is not None, "Observing object was not specified."
-    if OBJECT == 'sun':
-        RA, DEC = telescope.SUN_RA, telescope.SUN_DEC
-    elif OBJECT == 'sgr1935':
-        RA, DEC = telescope.SGR_RA, telescope.SGR_DEC
-    elif OBJECT == 'crab':
-        RA, DEC = telescope.CRAB_RA, telescope.CRAB_DEC
+    assert OBJECT in sources, 'Observing object was not specified.'
+    RA, DEC = sources[OBJECT]['RA'], sources[OBJECT]['DEC']
+
 if INPUT_RA and INPUT_DEC is not None:
     assert OBJECT is None, "Cannot give both input ra/dec values and specifiy an observing object."
     RA, DEC = INPUT_RA, INPUT_DEC
 
-
-t = telescope.Telescope()
 
 # Point to first coordinate values (alt, az)
 ALT0, AZ0 = t.calc_altaz(RA, DEC)
 print('Slewing...')
 t.point(ALT0, AZ0, wait=True, verbose=VERBOSE)
 
-# Start collecting data
-subprocess.run(['/usr/local/bin/enable_record.sh'], shell=True) # enable data recording
+t.track(RA, DEC, verbose=VERBOSE)
 
-print('Tracking '+ OBJECT)
+already_recording = False
 try:
-    t.track(RA, DEC, verbose=VERBOSE)
-except(AssertionError): # if object is out of bounds
-    print('Source is out of range. Finishing observation.')
+    while True:
+        record = r.hget('limbo:Record')
+        if record == 1 and not already_recording:
+            subprocess.run(['/usr/local/bin/enable_record.sh'], shell=True) # enable data recording
+            already_recording = True
+        if record == 0 and already_recording:
+            subprocess.run(['/usr/local/bin/disable_record.sh'], shell=True) # disable data recording
+            already_recording = False
+except(KeyboardInterrupt):
     t.stop()
     subprocess.run(['/usr/local/bin/disable_record.sh'], shell=True) # disable data recording
-    print('Stowing...')
     t.stow()
