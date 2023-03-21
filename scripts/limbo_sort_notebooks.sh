a3b9ea254680804e603d74f41f7491339da85d68
Darby <darbymccauley@berkeley.edu>
2023-03-21 16:23:34 +0000

    Bugfix to limbo_sort_notebooks

--- scripts/limbo_sort_notebooks.sh
+++ scripts/limbo_sort_notebooks.sh
@@ -6,13 +6,13 @@ cd ${DATAPATH}/notebook
 
 for f in `ls *.dat.ipynb`
 do
-    fdat=`echo Spectra_20230321152750.dat.ipynb | sed "s/.ipynb//"`
+    fdat=`echo ${f} | sed "s/.ipynb//"`
     if test -f "${DATAPATH}/save/${fdat}"; then
-	    echo Moving ${f} to ${DATAPATH}/notebook/save
-	    mv ${f} ${DATAPATH}/notebook/save
+        echo Moving ${f} to ${DATAPATH}/notebook/save
+        mv ${f} ${DATAPATH}/notebook/save
     elif test -f "${DATAPATH}/remove/${fdat}"; then
         echo Moving ${f} to ${DATAPATH}/notebook/remove
-	    mv ${f} ${DATAPATH}/notebook/remove
+        mv ${f} ${DATAPATH}/notebook/remove
     else
         echo Could not find ${fdat}
     fi
