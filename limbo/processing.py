b2b07addc08c0a85c5a4f6d02b403bc4700bd939
Aaron Parsons <aparsons@berkeley.edu>
2023-04-19 11:11:46 -0700

    Updated processing to use a new frequency mask and added hotspot flagging. Also tightened up noise statistics a little.

--- limbo/processing.py
+++ limbo/processing.py
@@ -8,43 +8,55 @@ BANDPASS_FILE = os.path.join(os.path.dirname(__file__),'data', 'bandpass_v002.np
 BANDPASS_NPZ = np.load(BANDPASS_FILE)
 FMDL = BANDPASS_NPZ['mdl']
 
-FREQMASK_FILE = os.path.join(os.path.dirname(__file__),'data', 'freq_mask_v001.npz')
+FREQMASK_FILE = os.path.join(os.path.dirname(__file__),'data', 'freq_mask_v002.npz')
 FREQ_MASK_NPZ = np.load(FREQMASK_FILE)
 FREQ_MASK = FREQ_MASK_NPZ['mask']
 FREQ_AMAT = FREQ_MASK_NPZ['amat']
 FREQ_FMAT = FREQ_MASK_NPZ['fmat']
-
-TIMEFILT_FILE = os.path.join(os.path.dirname(__file__),'data', 'time_filt_v001.npz')
-TIME_FILT_NPZ = np.load(TIMEFILT_FILE)
-TIME_AMAT = FREQ_MASK_NPZ['amat']
-TIME_FMAT = FREQ_MASK_NPZ['fmat']
 
 def dpss_filter(y, amat, fmat):
     '''Apply the provided DPSS filter matrices to data.'''
     model = amat @ (fmat @ y)
     return model.real
 
-def process_data(hdr, data, ch0=400, ch1=400+1024, nsig=4, maxdm=500,
-                 fmask=FREQ_MASK, freq_amat=FREQ_AMAT, freq_fmat=FREQ_FMAT,
-                 time_amat=TIME_AMAT, time_fmat=TIME_FMAT):
+def process_data(hdr, data, ch0=400, ch1=400+1024, gsig=4, maxdm=500,
+                 hch0=1173, hch1=1310, hsig=3,
+                 fmask=FREQ_MASK, freq_amat=FREQ_AMAT, freq_fmat=FREQ_FMAT):
+    '''Process LIMBO data by detrending, flagging, and performing a DM transform.
+    Arguments:
+        hdr: Header from LIMBO file
+        data: Data from LIMBO file.
+        ch0: Lower channel of window for performing DM transform.
+        ch1: Upper channel of window for performing DM transform.
+        gsig: Number of sigma for flagging gain variations
+        maxdm: Max DM in DM transform
+        hch0: Lower channel of "hot" RFI zone
+        hch1: Upper channel of "hot" RFI zone
+        hsig: Number of sigma for flagging "hot" zone excess power.
+        fmask: Frequency channel mask, derived from data/freq_mask_v002.npz
+        freq_amat: Frequency filtering design matrix, derived from data/freq_mask_v002.npz
+        freq_fmat: Frequency filtering matrix mask, derived from data/freq_mask_v002.npz
+    Returns:
+        dmt: Dictionary with keys 'dmt', 'dms', 'fmdl', 'tmdl', 'diff'.
+    '''
     spec = np.mean(data, axis=0)
     fmdl = dpss_filter(spec * fmask.astype('float'), freq_amat, freq_fmat)
-    tgain = np.sum(data[:,fmask], axis=1, keepdims=False) / np.sum(fmdl[fmask])
-    #tnos = np.sqrt(np.median(np.abs(tgain - 1)**2))  # XXX not realy std
-    tnos = np.median(np.abs(tgain - 1))  # not really std, just rough estimate
-    tmask = (tgain - 1) / tnos < nsig  # not really std, just rough estimate
-    tgain[~tmask] = 1
-    #tmdl = dpss_filter(tgain, time_amat, time_fmat)  # XXX too much work for payoff?
-    tmdl = tgain  # shortcut
+    # compute noise from smoothed spectrum; assumes same gain for all t in file
+    nos = fmdl / hdr['AccLen']**0.5
+    tnos = 1 / hdr['AccLen']**0.5 / (ch1-ch0)**0.5
+    tmdl = np.sum(data[:,ch0:ch1][:,fmask[ch0:ch1]], axis=1, keepdims=False) / np.sum(fmdl[ch0:ch1][fmask[ch0:ch1]])
+    tmask = np.zeros(tmdl.shape, dtype=bool)
+    tmask[1:-1] = np.abs(tmdl[1:-1] - 0.5*(tmdl[2:] + tmdl[:-2])) < gsig * (tnos * np.sqrt(1+2*0.5**2))
     mdl = np.outer(tmdl, fmdl)
     diff_data = data - mdl
-    #nos = mdl / hdr['AccLen']**0.5  # compute noise from power spectrum level
-    #full_mask = np.where(diff_data > nsig * nos, False, True)
-    full_mask = np.ones_like(diff_data).astype(bool)
-    full_mask[:, ~fmask] = False
-    full_mask[~tmask] = False
+    full_mask = np.outer(tmask, fmask)
     diff_data *= full_mask
-    fdmt = FDMT(hdr['freqs'][ch0:ch1], hdr['times'], maxDM=maxdm)
-    dm_vs_t = fdmt.apply(diff_data[:, ch0:ch1])
-    rv = {'dmt': dm_vs_t, 'dms': fdmt.dms, 'fmdl': fmdl, 'tmdl': tmdl, 'diff': diff_data}
-    return rv
+    hot = np.sum(diff_data[:,hch0:hch1], axis=1)
+    hnos = np.sqrt(np.sum(nos[hch0:hch1][fmask[hch0:hch1]]**2))
+    thot = np.where(hot > hsig * hnos)
+    diff_data[thot,hch0:hch1] = 0
+    full_mask[thot,hch0:hch1] = 0
+    fdmt = limbo.processing.FDMT(hdr['freqs'][ch0:ch1], hdr['times'], maxDM=maxdm)
+    dm_vs_t = fdmt.apply(diff_data[:,ch0:ch1])
+    dmt = {'dmt': dm_vs_t, 'dms': fdmt.dms, 'fmdl': fmdl, 'tmdl': tmdl, 'diff': diff_data}
+    return dmt
