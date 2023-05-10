08a58caf0ae4f554cc638bc02b411052585f6a3b
Darby <darbymccauley@berkeley.edu>
2023-05-10 22:03:56 +0000

    Changed file io to read timestamps from spectra, not from file creation date

--- limbo/io.py
+++ limbo/io.py
@@ -13,15 +13,25 @@ NCHAN_DEFAULT = 2048
 
 def _get_header_size(f):
     s = f.read(1)
-    f.seek(0,0)
+    f.seek(0, 0)
     if(s==b'{'):
         header_size = HEADER_SIZE
     else:
         header_size = struct.unpack('I',f.read(4))[0]
     return header_size
 
+def read_start_time(filename, dtype=np.dtype('<u4')):
+    '''Read sec, usec from first spectrum in file.'''
+    with open(filename, 'rb') as f:
+        header_size = _get_header_size(f)
+        f.seek(header_size, 0)
+        # timestamp is 32b zeros, 32b sec, 32b zeros, 32b usec
+        _, sec, _, usec = np.frombuffer(f.read(16), dtype=dtype)
+    return sec, usec
+
 def read_header(filename, lo_hz=1350e6, nchan=NCHAN_DEFAULT, infochan=12, dtype=np.dtype('>u2')):
     '''Read header from a limbo file.'''
+    sec, usec = read_start_time(filename)
     with open(filename, 'rb') as f:
         header_size = _get_header_size(f)
         h = f.read(header_size)
@@ -30,6 +40,8 @@ def read_header(filename, lo_hz=1350e6, nchan=NCHAN_DEFAULT, infochan=12, dtype=
         h['sample_clock'] = h.pop('SampleFreq') * 1e6
         h['freqs'] = utils.calc_freqs(h['sample_clock'], lo_hz, nchan)
         h['inttime'] = utils.calc_inttime(h['sample_clock'], h['AccLen'], nchan)
+        h['Time_created'] = h['Time']
+        h['Time'] = float(sec) + float(usec) * 1e-6
     size = os.path.getsize(filename)
     h['nspec'] = (size - header_size) // (dtype.itemsize * (nchan + infochan))
     return h
@@ -40,7 +52,7 @@ def read_raw_data(filename, nspec=-1, skip=0, nchan=NCHAN_DEFAULT, infochan=12, 
     with open(filename, 'rb') as f:
         header_size = _get_header_size(f)
         start = header_size + skip * dtype.itemsize * (nchan + infochan)
-        header = f.seek(start,1)
+        header = f.seek(start, 0)
         if nspec < 0:
             data = np.frombuffer(f.read(), dtype=dtype)
         else:
@@ -55,8 +67,7 @@ def read_file(filename, nspec=-1, skip=0, lo_hz=1350e6, nchan=NCHAN_DEFAULT, inf
     data = read_raw_data(filename, nspec=nspec, skip=skip, nchan=nchan, infochan=infochan, dtype=dtype)
     assert data.shape[0] > 0  # make sure we read some data
     data = data[:, infochan:]
-#     hdr['times'] = hdr['Time'] + np.arange(data.shape[0]) * hdr['inttime']
-    hdr['times'] = (hdr['Time'] - data.shape[0] * hdr['inttime']) + np.arange(data.shape[0]) * hdr['inttime'] # PS 'Time' = end time of file
+    hdr['times'] = hdr['Time'] + np.arange(data.shape[0]) * hdr['inttime']
     t = Time(hdr['times'], format='unix', scale='utc')
     hdr['jds'] = t.jd
     hdr['date'] = t.strftime('%Y-%m-%d %H:%M:%S')[0]
