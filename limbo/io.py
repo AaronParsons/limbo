a18c0e0e1ba1d3b7ad35563ea166c62c7e4bd1a1
Aaron Parsons <aparsons@berkeley.edu>
2023-06-12 14:05:19 -0700

    Fixed read_volt_file to make polarization fastest array axis; added read_volt_header function

--- limbo/io.py
+++ limbo/io.py
@@ -46,6 +46,23 @@ def read_header(filename, lo_hz=1350e6, nchan=NCHAN_DEFAULT, infochan=12, dtype=
     h['nspec'] = (size - header_size) // (dtype.itemsize * (nchan + infochan))
     return h
 
+def read_volt_header(filename, lo_hz=1350e6, nchan=NCHAN_DEFAULT, infochan=24,
+                     dtype=np.dtype('>u2'), npol=2):
+    '''Read header from a limbo file.'''
+    start_t = read_start_time(filename)
+    with open(filename, 'rb') as f:
+        header_size = _get_header_size(f)
+        h = f.read(header_size)
+        h = json.loads(h[:h.find(0x00)])
+        h['filename'] = filename
+        h['sample_clock'] = h.pop('SampleFreq') * 1e6
+        h['freqs'] = utils.calc_freqs(h['sample_clock'], lo_hz, nchan)
+        h['inttime'] = utils.calc_inttime(h['sample_clock'], 1, nchan)
+        h['Time_created'] = h['Time']
+        h['Time'] = start_t
+    size = os.path.getsize(filename)
+    h['nspec'] = (size - header_size) // (dtype.itemsize * (npol * nchan + infochan))
+    return h
 
 def read_raw_data(filename, nspec=-1, skip=0, nchan=NCHAN_DEFAULT, infochan=12, dtype=np.dtype('>u2')):
     '''Read raw data from a limbo file.'''
@@ -61,7 +78,8 @@ def read_raw_data(filename, nspec=-1, skip=0, nchan=NCHAN_DEFAULT, infochan=12, 
     data.shape = (-1, infochan + nchan)
     return data
 
-def read_file(filename, nspec=-1, skip=0, lo_hz=1350e6, nchan=NCHAN_DEFAULT, infochan=12, dtype=np.dtype('>u2')):
+def read_file(filename, nspec=-1, skip=0, lo_hz=1350e6, nchan=NCHAN_DEFAULT,
+              infochan=12, dtype=np.dtype('>u2')):
     '''Read header and data from a limbo file.'''
     hdr = read_header(filename, lo_hz=lo_hz, nchan=nchan)
     data = read_raw_data(filename, nspec=nspec, skip=skip, nchan=nchan, infochan=infochan, dtype=dtype)
@@ -73,13 +91,15 @@ def read_file(filename, nspec=-1, skip=0, lo_hz=1350e6, nchan=NCHAN_DEFAULT, inf
     hdr['date'] = t.strftime('%Y-%m-%d %H:%M:%S')[0]
     return hdr, data
 
-def read_volt_file(filename, nspec=-1, skip=0, lo_hz=1350e6, nchan=2*NCHAN_DEFAULT, infochan=24, dtype=np.dtype('>u1')):
+def read_volt_file(filename, nspec=-1, skip=0, lo_hz=1350e6, nchan=NCHAN_DEFAULT,
+                   infochan=24, dtype=np.dtype('>u1'), npol=2):
     '''Read header and data from a limbo file.'''
-    hdr = read_header(filename, lo_hz=lo_hz, nchan=nchan)
-    data = read_raw_data(filename, nspec=nspec, skip=skip, nchan=nchan, infochan=infochan, dtype=dtype)
+    hdr = read_volt_header(filename, lo_hz=lo_hz, nchan=nchan, npol=npol)
+    data = read_raw_data(filename, nspec=nspec, skip=skip, nchan=npol*nchan,
+                         infochan=infochan, dtype=dtype)
     assert data.shape[0] > 0  # make sure we read some data
     data = data[:, infochan:]
-    data.shape = (-1, 2, NCHAN_DEFAULT)
+    data.shape = (-1, NCHAN_DEFAULT, npol)  # polarization is the fastest array axis
     data_real = (data & 0xf0).view('>i1') >> 4
     data_imag = ((data << 4) & 0xf0).view('>i1') >> 4
     hdr['times'] = hdr['Time'] + np.arange(data.shape[0]) * hdr['inttime'] # VS 'Time' = start time of file
