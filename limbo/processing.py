e89e622efa21a89d13015f9c9af05082a32c13e0
Darby McCauley <darbymccauley@rals-MacBook-Pro.local>
2023-07-14 18:33:41 -0700

    add voltage processing pipeline

--- limbo/processing.py
+++ limbo/processing.py
@@ -1,6 +1,8 @@
 import numpy as np
 import os
 from .fdmt import FDMT
+from .io import read_volt_file
+from .utils import DM_delay
 
 PRECISION = 1
 
@@ -90,3 +92,77 @@ def process_data(hdr, data, ch0=400, ch1=400+1024, gsig=4, maxdm=500,
         dmt['dmt'] = dm_vs_t
         dmt['dms'] = fdmt.dms
     return dmt
+
+class ProcessVoltage:
+    def __init__(self, DM):
+        self.DM = DM
+    
+    def _get_volt_analysis_params(self, t_events, vhdr, pad=2000):
+        """
+        Get nessecary volt file params to be able to view relavent parts of pulse.
+        XXX Only works for events that are sequential. Will need to be revised to include
+            possibility of multiple events in one file. XXX
+        Arguments:
+            t_events: time of events in power spectra
+            vhdr: voltage file header
+            pad: amount to pad data by
+        Returns:
+            window: window containing length of pulse
+            skip: how many spectra to skip in voltage file glob
+        """
+        skip = np.floor((t_events - vhdr['Time']) / vhdr['inttime']).astype(int) - pad
+        max_delay = DM_delay(self.DM, vhdr['freqs'][0]) - DM_delay(self.DM, vhdr['freqs'][-1])
+        nspec_delay = max_delay / vhdr['inttime']
+        nspec_read = int(np.ceil(skip[-1] + nspec_delay) + 2*pad) # XXX
+        window = nspec_read - skip[0] # XXX
+        return window, skip[0]
+    
+    def find_volt_window(self, volt_files, t_events, vhdr, pad=2000):
+        """
+        Return the complex spectra that contains the length of the pulse.
+        """
+        window, skip = self._get_volt_analysis_params(t_events=t_events, vhdr=vhdr, pad=pad)
+        vnspec = vhdr['nspec']
+        volt_files = sorted(volt_files)
+        for i, file in enumerate(volt_files):
+            if skip > vnspec * (i+1):
+                continue
+            else:
+                _skip = skip - (vnspec * i)
+                if window < vnspec - _skip: # read to end of window
+                    _, data_real, data_imag = read_volt_file(file, skip=_skip, nspec=window)
+                elif window > vnspec - _skip: # if pulse stradles two files
+                    _, dr0, di0 = read_volt_file(file, skip=_skip, nspec=-1) # read to end of file
+                    _, dr1, di1 = read_volt_file(volt_files[i+1], skip=0, nspec=window - dr0.shape[0]) # read remaining amount from next file
+                    data_real = np.concatenate((dr0, dr1), axis=0)
+                    data_imag = np.concatenate((di0, di1), axis=0)
+                break
+        return data_real, data_imag
+    
+    def sum_pols(self, data_real, data_imag):
+        """ Sum polarizations """
+        p0 = data_real[:, :, 0]**2 + data_imag[:, :, 0]**2
+        p1 = data_real[:, :, 1]**2 + data_imag[:, :, 1]**2
+        return p0 + p1
+
+    def get_volt_streams(self, data_real, data_imag, dtype='complex64'):
+        """ Get indiviual voltage streams """
+        v0 = data_real[:, :, 0] + np.asarray(1j).astype(dtype) * data_imag[:, :, 0]
+        v1 = data_real[:, :, 1] + np.asarray(1j).astype(dtype) * data_imag[:, :, 1]
+        return v0, v1
+    
+    def sum_down(self, vdata, sum_int=128):
+        """ Sum voltage data along time axis. """
+        if vdata.shape[0] % sum_int != 0:
+            vdata = vdata[:-(vdata.shape[0] % sum_int)]
+        vdata.shape = (-1, sum_int, vdata.shape[1])
+        vdata = np.mean(vdata, axis=1)
+        return vdata
+
+    # XXX Figure out best way to compute Stoke params:
+    # def compute_stokes_params(volt0, volt1):
+    #     I = np.abs(volt0)**2 + np.abs(volt1)**2
+    #     Q = np.abs(volt0)**2 - np.abs(volt1)**2
+    #     U = volt0 * volt1.conj() + volt1 * volt0.conj()
+    #     V = volt0 * volt1.conj() - volt1 * volt0.conj()
+    #     return {'I':I, 'Q':Q, 'U':U, 'V':V}
